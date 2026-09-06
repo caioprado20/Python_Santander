e139e83f3dbf06327cb28304f91ef23115947a6e
caio-prado <caioprado20@gmail.com>
2026-09-06 10:45:57 -0300

    ...

---
+++ Modulo2/7_Dicionários.py
@@ -0,0 +1,4 @@
+## Dicionários
+### Dicionários são uma extrutura de dados mutável e não ordenada que permite armazenar pares de chave-valor. Cada elementeo em um dicionário consiste em uma chave única e seu valor corespondete. Os dicionários são delimitados por chaves{}, e os  pares chave-valor são separados por vírgula.
+
+### INICIALIZAÇÂO
